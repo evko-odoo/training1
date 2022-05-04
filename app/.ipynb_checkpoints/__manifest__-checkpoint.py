c0da4af1285639731be53acd8008daabe7d2ac2a
evko-odoo <evko@odoo.com>
2022-05-04 18:14:30 +0000

    [ADD]Basic Module

---
+++ app/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,21 @@
+# -*- coding: UTF-8 -*-
+{
+    'name': 'Test app',
+    'summary': """
+    
+    Test how this thing works
+    
+    """,
+    'author':'evko',
+    
+    'website':'',
+    
+    'category':"Training",
+    'version':'0.001',
+    'depends':['base'],
+    'data': [],
+    'demo': [],
+    
+    
+    
+}
